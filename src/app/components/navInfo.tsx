b45f238c66acce02f9a96f2ae32476df3fcaf342
Md. Nayeem <renayeem@gmail.com>
2023-09-01 23:27:12 +0600

    first nav info done

---
+++ src/app/components/navInfo.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../css/navInfo.css'
+
+const navInfo = () => {
+    return (
+        <div>
+            <div className='first-nav-info'>
+                Trained Aesop consultations are available to provide bespoke skin care advise. <span>Book a video consultation</span>
+            </div>
+        </div>
+    );
+};
+
+export default navInfo;
